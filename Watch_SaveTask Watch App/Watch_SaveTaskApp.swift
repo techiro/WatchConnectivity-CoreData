b24571caaa4e052926519991cf0ecc7d22fc9159
techiro <techiromen0@gmail.com>
2022-09-25 14:55:01 +0900

    Initial Commit

---
+++ Watch_SaveTask Watch App/Watch_SaveTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_SaveTaskApp.swift
+//  Watch_SaveTask Watch App
+//
+//  Created by TanakaHirokazu on 2022/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Watch_SaveTask_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Watch_SaveTask/Watch_SaveTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_SaveTaskApp.swift
+//  Watch_SaveTask
+//
+//  Created by TanakaHirokazu on 2022/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Watch_SaveTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
